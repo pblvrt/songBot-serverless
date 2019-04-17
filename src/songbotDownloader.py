b650114207166549bac3daaeb916d24ee3acc56a
pabloVoorvaart <pablov@graficadirecta.com>
2019-04-18 00:30:15 +0200

    finished songbotDownloader

--- src/songbotDownloader.py
+++ src/songbotDownloader.py
@@ -7,20 +7,34 @@ s3client = boto3.resource('s3')
 
 def songbotDownloader(event, context):
     service = event['service']
-    songTitle = event['songTitle']
+    songTitle = event['songTitle'].replace(" ", "_")
     songUrl = event['songUrl']
     print(event)
 
+
+    class MyLogger(object):
+        def debug(self, msg):
+            pass
+
+        def warning(self, msg):
+            pass
+
+        def error(self, msg):
+            print(msg)
+
+
+    def my_hook(d):
+        print(d)
+        if d['status'] == 'finished':
+            print('Done downloading, now uplaoding ...')
+            s3client.Object(os.environ['s3bucket'], songTitle.replace("_", " ")).put(Body=open(d['filename'], 'rb'))
+
     ydl_opts = {
         'format': 'bestaudio/best',
-        'outtmpl': '/tmp/%(id)s.%(ext)s',
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
-            'preferredquality': '320',
-        }],
+        'outtmpl': '/tmp/'+songTitle+'.%(ext)s',
+        'logger': MyLogger(),
+        'progress_hooks': [my_hook],
     }
 
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([songUrl])
-    s3client.Object(os.environ['s3bucket'], songTitle +'mp3').put(Body=open('tmp/'+ songTitle +'.mp3', 'rb'))
+        ydl.download([songUrl])  
