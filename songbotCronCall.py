69600943d1c9dcef7aef98c177ebff30a3396534
pabloVoorvaart <pablov@graficadirecta.com>
2019-04-17 23:03:56 +0200

    finished songbotCronCall.py

--- songbotCronCall.py
+++ songbotCronCall.py
@@ -8,87 +8,84 @@ s3 = boto3.resource('s3')
 lambdaClient = boto3.client('lambda')
 
 
-def songBotCronCall(event, context):
+def songbotCronCall(event, context):
 
     songList = []
     class Song:
-        def __init__(self, id, title, url):
-            self.id = id
+        def __init__(self, service, title, url):
+            self.service = service
             self.title = title
             self.url = url
 
-    # SoundCloud
+    #################### soundcloud ####################
+    # SoundCloud fixed variables
     soundcloudUserId = 'users/580297311/playlists'
-    soundcloudClientId = '?client_id=xIa292zocJP1G1huxplgJKVnK0V3Ni9D'
     soundcloudApiEndpoint = 'http://api.soundcloud.com/'
 
-    soundCloudDownloadAllPlaylist = True
-    playlistToDownload = []
+    # SoundCloud env variables loaded
+    soundcloudClientId = '?client_id=' + os.environ['soundcloudClientId']
+    soundcloudDownloadAllPlaylist = os.environ['soundcloudDownloadAllPlaylist']
+    soundcloudPlaylistToDownload = os.environ['soundcloudPlaylistToDownload']
 
-    if soundCloudDownloadAllPlaylist:
-        #get all playlist and then tracks
+    #get all playlist and then tracks
+    if soundcloudDownloadAllPlaylist == 'True':
         requestResponse = requests.get(soundcloudApiEndpoint + soundcloudUserId + soundcloudClientId)
-        #requestResponse = requests.get('http://api.soundcloud.com/users/580297311/playlists?client_id=xIa292zocJP1G1huxplgJKVnK0V3Ni9D')
         jsonResponse = requestResponse.json()
-
         for playlist in jsonResponse:
             for track in playlist['tracks']:
-                songList.append(Song(None, track['title'], track['permalink_url']))
-                
+                songList.append(Song('soundcloud', track['title'], track['permalink_url']))              
     else:
-        if playlistToDownload:
-            for playlistId in playlistToDownload:
+        if soundcloudPlaylistToDownload:
+            playlistIds = soundcloudPlaylistToDownload.split('|')
+            for playlistId in playlistIds:
                 requestResponse = requests.get(soundcloudApiEndpoint + playlistId + 'playlists' + soundcloudClientId)
-                #requestResponse = requests.get('http://api.soundcloud.com/playlists?client_id=xIa292zocJP1G1huxplgJKVnK0V3Ni9D')
                 jsonResponse = requestResponse.json()
-
         else:
             print('no playlist to download.')
 
 
-    #  YouTube
-    key = os.environ['key']
-    youtubeChannelId = os.environ['youtubeChannelId']
+    ####################  YouTube ####################
+    # youTube fixed variables
     youtubeApiEndpoint = 'https://www.googleapis.com/youtube/v3/'
 
-    youtubeDonwloadAllPlaylist = False
-    playlistToDownload = ['PL79EnF5Uf84FJ__-mhSEpWgKSUXX0cv-P']
+    # youTube env variables loaded
+    youtubeChannelId = os.environ['youtubeChannelId']
+    youtubeApiKey = os.environ['youtubeApiKey']
+    youtubeDonwloadAllPlaylist = os.environ['youtubeDonwloadAllPlaylist']
+    youtubePlaylistToDownload = os.environ['youtubePlaylistToDownload']
 
-    if youtubeDonwloadAllPlaylist:
-        pass
+    if youtubeDonwloadAllPlaylist == 'True':
+        pass       
     else:
-        if playlistToDownload:
-            for playlistId in playlistToDownload:
-                payload = {'part':'snippet', 'maxResults':'50', 'playlistId':playlistId, 'key':key}
+        if youtubePlaylistToDownload:
+            playlistIds = youtubePlaylistToDownload.split('|')
+            for playlistId in playlistIds:
+                payload = {'part':'snippet', 'maxResults':'50', 'playlistId':playlistId, 'key':youtubeApiKey}
                 requestResponse = requests.get(youtubeApiEndpoint + 'playlistItems', params=payload)
                 jsonResponse = requestResponse.json()
-
                 for song in jsonResponse['items']:
                     youtubeSongId = song['snippet']['resourceId']['videoId']
                     youtubeSongTitle = song['snippet']['title']
-                    songList.append(Song(youtubeSongId, youtubeSongTitle, None))
-
+                    songList.append(Song('youtube', youtubeSongTitle, 'https://www.youtube.com/watch?v=' + youtubeSongId))
         else:
             print('No playlist to download')
 
 
     # Check s3 for repeats
-    s3bucket = s3.Bucket(name='yt-mp3-storage')
-
+    s3bucket = s3.Bucket(name=os.environ['s3bucket'])
     bucketList = []
 
     for item in s3bucket.objects.all():
         bucketList.append(item.key[:-4])
-
     for song in songList:
         if song.title not in bucketList:
-            lambdaPayload = {'songID': song.id, 'songTitle': song.title, 'songUrl': song.url}
+            lambdaPayload = {'service': song.service, 'songTitle': song.title, 'songUrl': song.url}
             invoke = lambdaClient.invoke(
-                FunctionName='youtube-download-bot-dev-download',
+                FunctionName=os.environ['downloadLambdafunction'],
                 InvocationType='Event',
-                Payload=json.dumps(payload)
+                Payload=json.dumps(lambdaPayload)
             )
             print(invoke)
 
 
-            
+        
--- songbotDownloader.py
+++ songbotDownloader.py
@@ -5,23 +5,22 @@ import boto3
 
 s3client = boto3.resource('s3')
 
-def yt_downloader(event, context):
+def songbotDownloader(event, context):
+    service = event['service']
+    songTitle = event['songTitle']
+    songUrl = event['songUrl']
     print(event)
-    yt_id = event['yt_id']
-    title = event['title']
-    print(title + '' + title)
-    
+
     ydl_opts = {
-    'format': 'bestaudio/best',
-    'outtmpl': '/tmp/%(id)s.%(ext)s',
-    'postprocessors': [{
-        'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'mp3',
-        'preferredquality': '320',
-    }],
+        'format': 'bestaudio/best',
+        'outtmpl': '/tmp/%(id)s.%(ext)s',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '320',
+        }],
     }
 
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        meta = ydl.download(['https://www.youtube.com/watch?v=' + yt_id])
-
-    s3client.Object('media-convert-outputs', title +'mp3').put(Body=open('tmp/'+ yt_id+'.mp3', 'rb'))
+        ydl.download([songUrl])
+    s3client.Object(os.environ['s3bucket'], songTitle +'mp3').put(Body=open('tmp/'+ songTitle +'.mp3', 'rb'))
