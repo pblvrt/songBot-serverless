1a68b2c09bcab2f6db5951bc5f82417e9b29b098
pabloVoorvaart <pablov@graficadirecta.com>
2019-04-18 12:46:55 +0200

    finished project for now

--- src/songbotDownloader.py
+++ src/songbotDownloader.py
@@ -6,12 +6,12 @@ import boto3
 s3client = boto3.resource('s3')
 
 def songbotDownloader(event, context):
-    service = event['service']
+    service = event['service'] # service can be youtube or soundcloud
     songTitle = event['songTitle'].replace(" ", "_")
     songUrl = event['songUrl']
     print(event)
 
-
+    # Logger class for youtubeDl
     class MyLogger(object):
         def debug(self, msg):
             pass
@@ -23,17 +23,20 @@ def songbotDownloader(event, context):
             print(msg)
 
 
-    def my_hook(d):
-        print(d)
-        if d['status'] == 'finished':
+    def hook(job):
+        if job['status'] == 'finished':
             print('Done downloading, now uplaoding ...')
-            s3client.Object(os.environ['s3bucket'], songTitle.replace("_", " ")).put(Body=open(d['filename'], 'rb'))
+            try:
+                s3client.Object(os.environ['s3bucket'], songTitle.replace("_", " ")).put(Body=open(job['filename'], 'rb'))
+            except:
+                print("Uplaod filed")
+                return                
 
     ydl_opts = {
         'format': 'bestaudio/best',
         'outtmpl': '/tmp/'+songTitle+'.%(ext)s',
         'logger': MyLogger(),
-        'progress_hooks': [my_hook],
+        'progress_hooks': [hook],
     }
 
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
